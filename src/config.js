ee3049e84454e23ca978853674920f5a3a9a07e9
Steven Wagner <steven.glen.wagner@gmail.com>
2019-08-06 13:34:07 -0600

    Added production url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-        "http://localhost:8000/api",
+    API_BASE_URL: "https://cryptic-depths-79803.herokuapp.com/api",
     TOKEN_KEY: 'authToken',
     user_id: 'user_id'
 };
