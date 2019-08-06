8e6f762d58c53de1166b40a5945e3af74b705e9d
Steven Wagner <steven.glen.wagner@gmail.com>
2019-08-06 13:30:29 -0600

    add env variables

--- src/config.js
+++ src/config.js
@@ -1,12 +1,3 @@
-let url;
-
-if (process.env.NODE_ENV !== 'production') {
-    url = "http://localhost:8000/api"
-}
-else {
-    url = "https://cryptic-depths-79803.herokuapp.com/api"
-}
-
 module.exports = {
     PORT: process.env.PORT || 8000,
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
