7f386c033d5ad65a384ddcd441e490c1d5f1e1ae
Steven Wagner <steven.glen.wagner@gmail.com>
2019-08-12 15:54:34 -0600

    Render tests for yourProduct

--- src/YourProducts/YourProduct/yourProduct.test.js
+++ src/YourProducts/YourProduct/yourProduct.test.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import YourProduct from './yourProduct';
 import Enzyme, { shallow, mount, render, setState, update } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
-import {ProductsProvider} from '../context';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faShoppingCart, faSpinner, faCoins } from '@fortawesome/free-solid-svg-icons';
 import ReactDOM from 'react-dom';
@@ -29,12 +28,15 @@ describe('Homepage UI renders correctly', () => {
         'Homepage ads': '1'
     }
 
+    const handleChangeInput = jest.fn();
+
     function makeYourProductWrapper() {
         return mount(
                 <Router>
                     <YourProduct
                         item={item}
-                        adCosts={adCosts}/>
+                        adCosts={adCosts}
+                        handleChangeInput={handleChangeInput}/>
                 </Router>)
     }
     
@@ -42,13 +44,56 @@ describe('Homepage UI renders correctly', () => {
       const div = document.createElement('div');
       ReactDOM.render(
         <Router>
-            {/* <ProductsProvider value={}> */}
                 <YourProduct
                     item={item}
                     adCosts={adCosts}/>
-            {/* </ProductsProvider> */}
         </Router> , div);
     });
+
+    describe('Img displayed', () => {
+        it('img does not display if url is undefined', () => {
+            const emptyImgItem = Object.assign({}, item);
+            emptyImgItem.img = '';
+            const wrapper = mount(
+                <Router>
+                    <YourProduct
+                        item={emptyImgItem}
+                        adCosts={adCosts}
+                        handleChangeInput={handleChangeInput}/>
+                </Router>);
+            expect(wrapper.exists('product-pic')).toBe(false);
+            wrapper.unmount();
+        })
+        it('img displays if url is present', () => {
+            const wrapper = makeYourProductWrapper();
+            expect(wrapper.exists('.product-pic')).toBe(true);
+            wrapper.unmount();
+        })
+    })
+
+    describe('edit popups render correctly', () => {
+        const editTypes = ['title', 'img', 'description'];
+
+        it('Edit popup does not render on page load', () => {
+            const wrapper = makeYourProductWrapper();
+            expect(wrapper.exists('EditPopup')).toBe(false);
+            wrapper.unmount();
+        })
+
+        editTypes.forEach(type => {
+            it(`${type} edit popup renders`, () => {
+                const wrapper = makeYourProductWrapper();
+                wrapper.find('YourProduct').setState({
+                    editPopup: {
+                        type: type,
+                        status: true
+                    }
+                })
+                expect(wrapper.exists('EditPopup')).toBe(true);
+                wrapper.unmount();
+            })
+        })
+    })
 
     // it('Homepage renders users top sellers correctly', () => {
     //     const userPopularProduct = makeHomepageWrapper().find('.top-selling-item').at(0);
